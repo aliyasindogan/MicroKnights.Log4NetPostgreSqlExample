0c87845a0ffb989af8183b521954eab077c1c0f1
aliyasindogan <a.yasindogan@gmail.com>
2020-12-26 12:09:28 +0300

    Genel düzenleme yapıldı.

--- Log4NetExample/Program.cs
+++ Log4NetExample/Program.cs
@@ -1,17 +1,12 @@
-﻿using DataAccess;
-using log4net;
+﻿using log4net;
 using log4net.Config;
-using System;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 
 namespace Log4NetExample
 {
     internal class Program
     {
-        private log4net.ILog log;
-
         private static void Main(string[] args)
         {
             //using (PostgreSqlDbContext context = new PostgreSqlDbContext())
@@ -23,7 +18,7 @@ namespace Log4NetExample
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
             var logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-            logger.Error("Hello World!");
+            logger.Error("Hello World111!");
         }
     }
 }
